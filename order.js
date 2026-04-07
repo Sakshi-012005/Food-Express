e928ea96f0f0444abf077aa3a43a948432d332cd
Sakshi-012005 <sakshikamanache465@gmail.com>
2026-04-07 20:23:34 +0530

    Add files via upload

---
+++ order.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const OrderSchema = new mongoose.Schema({
+    items: Array,
+    total: Number,
+    createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("Order", OrderSchema);
